d79aeb0a54159186b3a3c73d4aa55478bb9baa99
Smriti Khanal <ghimirerasmi9898@gmail.com>
2026-01-04 09:42:44 +0545

    Added cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from db import create_db_and_tables 
-from routers import users, verification, projects, issues, expenses
+
+from db import create_db_and_tables
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import expenses, issues, projects, users, verification
 
 
 @asynccontextmanager
@@ -10,6 +12,7 @@ async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
 
+
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(users.router, prefix="/user", tags=["users"])
@@ -17,6 +20,15 @@ app.include_router(verification.router, prefix="/verification", tags=["verify"])
 app.include_router(projects.router, prefix="/projects", tags=["projects"])
 app.include_router(issues.router, prefix="/issues", tags=["issues"])
 app.include_router(expenses.router, prefix="/expenses", tags=["expenses"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.head("/")
 def read_root():
